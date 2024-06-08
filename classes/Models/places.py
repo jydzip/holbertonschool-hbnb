e0570b7469cbf5f14c375bf7b7482f958791cbff
my-holber <8762@holbertonstudents.com>
2024-06-08 20:08:22 +0400

    J'avais oublié de sauvegarder un fichier -> 'Places.py'

--- classes/Models/places.py
+++ classes/Models/places.py
@@ -1,15 +1,19 @@
 class Places:
-    def __init__(self, id:int, name:str, adress:str, city_id:int, 
-                 host_id:str, number_of_rooms:int, number_of_bathrooms:int,
-                 price_per_night:int, max_guests:int, amenity_ids:list):
+    def __init__(self, data:dict):
         
-        self.id = id
-        self.nane = name
-        self.adress = adress
-        self.city_id = city_id
-        self.host_id = host_id
-        self.number_of_rooms = number_of_rooms
-        self.number_of_bathrooms = number_of_bathrooms
-        self.price_per_night = price_per_night
-        self.max_guests = max_guests
-        self.amenity_ids = amenity_ids
+        self.id = data['id']
+        self.name = data['name']
+        self.adress = data['adress']
+        self.city_id = data['city_id']
+        self.host_id = data['host_id']
+        self.number_of_rooms = data['number_of_rooms']
+        self.number_of_bathrooms = data['number_of_bathrooms']
+        self.price_per_night = data['price_per_night']
+        self.max_guests = data['max_guests']
+        self.amenity_ids = data['amenity_ids']
+
+    def __str__(self) -> str:
+        return (f"[Place] {self.id} _ {self.name} /\ {self.adress} /\ 
+                {self.city_id} /\ {self.host_id} /\ {self.number_of_rooms} /\ 
+                {self.number_of_bathrooms} /\ {self.price_per_night} /\ 
+                {self.max_guests} /\ {self.amenity_ids}")
