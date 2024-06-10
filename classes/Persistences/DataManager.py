3636088649e930ec2a0c0b8401a3b4ffe8555f62
Loïc Assael <8761@holbertonstudents.com>
2024-06-10 15:55:24 +0200

    DataManager.py

--- classes/Persistences/DataManager.py
+++ classes/Persistences/DataManager.py
@@ -32,9 +32,26 @@ class DataManager(IPersistenceManager):
                 return None
             return self._TABLE_CLASS(data)
 
-    def _update(self, entity):
-        # Logic to update an entity in storage
-        pass
+    def _update(self, entity: dict):
+        with open(self._TABLE_PATH, 'r', encoding="utf-8") as file:
+            datas: dict = json.load(file)
+            entity_id = entity.get("id")
+            if entity_id not in datas:
+                raise TypeError ("not id")
+
+            entity_attrs = {attr: typ for attr, typ in self._TABLE_CLASS.__annotations__.items()}
+            for key, value in entity.items():
+                if key not in entity_attrs:
+                    raise TypeError ("key not in entity_attrs")
+                if not isinstance(value, entity_attrs):
+                    raise ValueError ("the value is not str")
+            
+            for key, value in entity.items():
+                # datas[1]["name"] = "Loic"
+                datas[entity_id][key] = value
+
+            with open(self._TABLE_PATH, "w", encoding="utf-8") as file:
+                json.dump(datas, file, indent=4)
 
     def _delete(self, entity_id, entity_type=None):
         with open(self._TABLE_PATH, 'r', encoding="utf-8") as file:
